0489fe12c223020cc894afd42e0854b280de212c
just_flash_kit <mhoanga1@gmail.com>
2025-06-18 20:40:26 +0700

    add auth page, next: create account and sign in modal

---
+++ src/components/auth/AuthButton.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button'
+
+interface props {
+  content: string
+  onClick?: () => void
+}
+
+export default function AuthButton({ content, onClick }: props) {
+  return (
+    <Button
+      onClick={onClick}
+      className="rounded-4xl w-3xs my-1 hover:bg-iconground hover:text-black bg-primary text-white"
+    >
+      {content}
+    </Button>
+  )
+}
