ff9fbbd17fc5db57eb1010cca934bb13c80fc2c9
just_flash_kit <mhoanga1@gmail.com>
2025-07-02 15:12:52 +0700

    add login modal and register modal

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/login', request.url));
+    return NextResponse.redirect(new URL('/auth', request.url));
   }
   return NextResponse.next();
 }
